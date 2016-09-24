d3ab0d055c3997a52913da0d97dbfeac19b434fc
lesguillemets <lesguillemets@users.noreply.github.com>
2016-09-25 02:24:35 +0900

    add doctest

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,25 +11,34 @@ import Data.ByteString.Char8 (ByteString)
 import Data.Monoid ((<>))
 import Data.Attoparsec.ByteString as AB
 import Data.Attoparsec.ByteString.Char8 as ABC
+import Data.List (intercalate)
 
 type IPAddress = ByteString
 type Path = ByteString
 type Version = Int
 
 ip :: Parser BC.ByteString
-ip = BC.pack . concat <$> many1 digit `sepBy1` char '.'
+ip = BC.pack . intercalate "." <$> (many1 digit `sepBy1` char '.')
+-- |
+-- >>> parseOnly ip . BC.pack $ "127.0.0.1"
+-- Right "127.0.0.1"
 
 -- TODO : better considerations
 path :: Parser BC.ByteString
 path = BC.pack <$> (char '/' *>  (anyChar `manyTill'` char '?'))
+-- |
+-- >>> parseOnly path . BC.pack $ "/there/is/a/pen?v=3"
+-- Right "there/is/a/pen"
 
 version :: Parser Int
 version = string "version=" *> (read <$> many1 digit)
 
 originUrl :: Parser Voice
-originUrl =
-    Voice <$>
-         ((skipMany anyChar *> string "http://") *> ip) <*> path <*> version
+originUrl = Voice <$> (string "http://" *> ip) <*> path <*> version
+
+-- |
+-- >>> parseOnly originUrl . BC.pack $ "http://2.1.9.1/p/s/rr/1.mp3?version=8"
+-- Right (Voice "2.1.9.1" "p/s/rr/1.mp3" 8)
 
 data Voice = Voice IPAddress Path Version deriving (Show)
 toUrl :: Voice -> ByteString
