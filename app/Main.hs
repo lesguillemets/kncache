3d115182467c75bbaeaf5a1de63e0a2ff61bf89a
lesguillemets <lesguillemets@users.noreply.github.com>
2016-09-25 02:02:05 +0900

    initial tryal

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Lib
+import Data.Attoparsec.ByteString (parse)
+import qualified Data.ByteString as B
+import System.Environment (getArgs)
 
 main :: IO ()
-main = return ()
+main = do
+    (f:_) <- getArgs
+    print . parse originUrl =<< B.readFile f
--- src/Lib.hs
+++ src/Lib.hs
@@ -4,15 +4,38 @@ module Lib
         originUrl
     ) where
 
+import Control.Applicative
 import qualified Data.ByteString.Lazy.Char8 as BLC
 import qualified Data.ByteString.Char8 as BC
+import Data.ByteString.Char8 (ByteString)
 import Data.Monoid ((<>))
-import Data.Attoparsec.ByteString
-import Data.Attoparsec.ByteString.Char8
+import Data.Attoparsec.ByteString as AB
+import Data.Attoparsec.ByteString.Char8 as ABC
 
-originUrl :: Parser BC.ByteString
-originUrl = do
-    http <- string "http"
-    content <- many' (char '.')
-    return $ http <> BC.pack content
+type IPAddress = ByteString
+type Path = ByteString
+type Version = Int
 
+ip :: Parser BC.ByteString
+ip = BC.pack . concat <$> many1 digit `sepBy1` char '.'
+
+-- TODO : better considerations
+path :: Parser BC.ByteString
+path = BC.pack <$> (char '/' *>  (anyChar `manyTill'` char '?'))
+
+version :: Parser Int
+version = string "version=" *> (read <$> many1 digit)
+
+originUrl :: Parser Voice
+originUrl =
+    Voice <$>
+         ((skipMany anyChar *> string "http://") *> ip) <*> path <*> version
+
+data Voice = Voice IPAddress Path Version deriving (Show)
+toUrl :: Voice -> ByteString
+toUrl (Voice addr p v) =
+    BC.concat [ "http://"
+              , addr
+              , p
+              , BC.pack . show $ v
+              ]
