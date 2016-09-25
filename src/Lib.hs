2fbf44e74ea704cf62b5e005c87fc3d72f3f9fb1
lesguillemets <lesguillemets@users.noreply.github.com>
2016-09-26 01:14:04 +0900

    filename

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,7 +2,12 @@
 module Lib where
 
 import Data.ByteString.Char8 (ByteString)
+import Data.Char (isDigit)
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Text.Encoding (decodeUtf8)
 import qualified Data.ByteString.Char8 as BC
+import Data.Monoid
 
 type IPAddress = ByteString
 type Path = ByteString
@@ -16,3 +21,11 @@ toUrl (Voice addr p v) =
               , p
               , BC.pack . show $ v
               ]
+
+toFileName :: Voice -> Text
+toFileName (Voice ip path v) =
+    fname <> ".v" <> version <> ".mp3"
+    where
+        fname =
+            T.takeWhile isDigit . last . T.split (== '/') . decodeUtf8 $ path
+        version = T.pack . show $ v
